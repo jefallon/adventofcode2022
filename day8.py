3cf30e638508db71967bb7fcb6d6db8c250e6f37
Kali <kali@locahost>
2022-12-08 23:12:45 -0500

    Adding day 8 inputs and solutions

--- day8.py
+++ day8.py
@@ -1,20 +1,82 @@
 import argparse
 
 def get_grid(infile):
-        cols = []
-        rows = []
+    cols = []
+    rows = []
     with open(infile, 'r') as f:
         for line in f:
             rows.append(line.strip())
+    cols = [''.join(s[i] for s in rows) for i in range(len(rows))]
+    return rows, cols
         
 
 
 def part1(infile):
-    grid = get_grid(infile)
+    rows, cols = get_grid(infile)
+    nrows = len(rows)
+    ncols = len(cols)
+    visible = 2*nrows+2*ncols - 4
+    for r in range(1,nrows-1):
+        for c, s in enumerate(rows[r][1:-1],1):
+            if all(x < s for x in rows[r][:c]) or all(x < s for x in rows[r][c+1:]):
+                visible += 1
+                continue
+            if all(x < s for x in cols[c][:r]) or all (x < s for x in cols[c][r+1:]):
+                visible += 1
+                continue
+    return visible
+
+def part2(infile):
+    rows, cols = get_grid(infile)
+    nrows = len(rows)
+    ncols = len(cols)
+    scenic = 0
+    for r in range(1,nrows - 1):
+        for c,s in enumerate(rows[r][1:-1],1):
+            east = 0
+            west = 0
+            south = 0
+            north = 0
+            te = c+1
+            tw = c-1
+            ts = r+1
+            tn = r-1
+            while te < ncols:
+                east += 1
+                if rows[r][te] < s:
+                    te += 1
+                else:
+                    break
+            if east:
+                while tw > -1:
+                    west += 1
+                    if rows[r][tw] < s:
+                        tw -= 1
+                    else:
+                        break
+                if west:
+                    while ts < nrows:
+                        south += 1
+                        if rows[ts][c] < s:
+                            ts += 1
+                        else:
+                            break
+                    if south:
+                        while tn > -1:
+                            north += 1
+                            if rows[tn][c] < s:
+                                tn -=1
+                            else:
+                                break
+            view = east*west*north*south
+            scenic = max(view,scenic)
+    return scenic
 
 def main():
     if args.p == 1:
         print(part1(args.f))
+    elif args.p == 2:
+        print(part2(args.f))
     else:
         raise NotImplementedError('Solution for that part has not been implemented')
 
